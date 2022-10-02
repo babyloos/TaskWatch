ad0b1568de5a980614b404bfb0fdaaf23d073dcf
babyloos <babyloos2501@gmail.com>
2022-10-02 15:52:22 +0900

    realm導入

--- App.tsx
+++ App.tsx
@@ -3,40 +3,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet} from 'react-native';
 import { MenuProvider } from 'react-native-popup-menu';
+import { Realm, createRealmContext} from '@realm/react';
+
 import TaskListScreen from './src/screens/TaskListScreen';
 import ProjectEditScreen from './src/screens/ProjectEditScreen';
 
 const Stack = createNativeStackNavigator();
+const {RealmProvider, useRealm, useQuery} = createRealmContext()
 
 const App = () => {
   return (
     <MenuProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="TaskList">
-          <Stack.Screen
-            name="TaskList"
-            component={TaskListScreen}
-            options={{ 
-              title: 'プロジェクト/タスク一覧',
-              headerStyle: {
-                backgroundColor: '#61adf5',
-              },
-              contentStyle: styles.body,
-            }}
-          />
-          <Stack.Screen 
-            name="ProjectEdit"
-            component={ProjectEditScreen}
-            options={{
-              title: 'プロジェクト編集',
-              headerStyle: {
-                backgroundColor: '#61adf5',
-              },
-              contentStyle: styles.body,
-            }}
-          />
-          </Stack.Navigator>
-      </NavigationContainer>
+      <RealmProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="TaskList">
+            <Stack.Screen
+              name="TaskList"
+              component={TaskListScreen}
+              options={{ 
+                title: 'プロジェクト/タスク一覧',
+                headerStyle: {
+                  backgroundColor: '#61adf5',
+                },
+                contentStyle: styles.body,
+              }}
+            />
+            <Stack.Screen 
+              name="ProjectEdit"
+              component={ProjectEditScreen}
+              options={{
+                title: 'プロジェクト編集',
+                headerStyle: {
+                  backgroundColor: '#61adf5',
+                },
+                contentStyle: styles.body,
+              }}
+            />
+            </Stack.Navigator>
+        </NavigationContainer>
+      </RealmProvider>
     </MenuProvider>
   );
 }
