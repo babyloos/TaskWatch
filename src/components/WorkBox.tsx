a93ddc10af00d07ae9dc4993cbc6d88cad7c9c70
babyloos <babyloos2501@gmail.com>
2022-10-21 16:33:45 +0900

    WorkBoxの表示内容を微修正

--- src/components/WorkBox.tsx
+++ src/components/WorkBox.tsx
@@ -67,8 +67,8 @@ const TaskBox = ({ navigation, work, editable}: PropType) => {
             angleCenter={{ x: 0.5, y: 0.5 }}
             style={[styles.buttonFace, buttonFaceStyle]}>
           <View style={styles.infoArea}>
-            <Text>開始: {getDateFormated(work.startTime)}</Text>
-            <Text>終了: {getDateFormated(work.endTime)}</Text>
+            <Text>開始:        {getDateFormated(work.startTime)}</Text>
+            <Text>終了:        {getDateFormated(work.endTime)}</Text>
             <Text>作業時間: {toDispTime(work.workTime)}</Text>
           </View>
           <View style={styles.editArea}>
